8021ddf0599dd2c0922597ef3444d74f8cdeba54
rendy adidarma <rendyadidarma@hotmail.com>
2022-04-05 13:49:20 +0700

    fragment

--- app/src/main/java/com/example/temperaturemontoringapps/MainActivity.java
+++ app/src/main/java/com/example/temperaturemontoringapps/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.temperaturemontoringapps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,4 +30,27 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.item_menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.alert_item:
+                
+                break;
+
+            case R.id.temperature_item:
+                Toast.makeText(this, "Temperature", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_main, fragment);
+        ft.commit();
+    }
+
 }
