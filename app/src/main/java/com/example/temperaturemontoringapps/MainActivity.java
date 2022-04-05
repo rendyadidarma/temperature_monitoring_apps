0a953cca46803a5447839e3312ba0bdfe03ab5e3
rendy adidarma <rendyadidarma@hotmail.com>
2022-04-05 10:36:18 +0700

    toolbar update

--- app/src/main/java/com/example/temperaturemontoringapps/MainActivity.java
+++ app/src/main/java/com/example/temperaturemontoringapps/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.temperaturemontoringapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.item_menu, menu);
+        return true;
     }
 }
